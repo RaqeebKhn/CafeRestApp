119fb0bf44ab6b87bbb1dbd6d52e842f2b48afcd
RaqeebKhn <joysouls.rk@gmail.com>
2025-06-02 00:50:40 +0530

    final oush 6.0

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const Table = require('./models/Tables');
 
 dotenv.config();
 
@@ -12,7 +11,6 @@ const tableRoutes = require("./routes/tableRoutes");
 const app = express();
 const port = 5000;
 
-// Middlewares
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
@@ -20,49 +18,18 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(express.json());
 
-// Seeder
-async function seedDefaultTables() {
-  const existingCount = await Table.countDocuments();
-  if (existingCount < 14) {
-    const tablesToCreate = [];
-    for (let i = existingCount + 1; i <= 14; i++) {
-      tablesToCreate.push({
-        id: i.toString(),
-        customers: 0
-      });
-    }
+mongoose.connect(process.env.BACKEND_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Could not connect to MongoDB:", err));
 
-    await Table.insertMany(tablesToCreate);
-    console.log(`${14 - existingCount} default tables inserted.`);
-  } else {
-    console.log('Default tables already seeded.');
-  }
-}
+app.use("/tables", tableRoutes);
 
-// Main setup
-async function startServer() {
-  try {
-    await mongoose.connect(process.env.BACKEND_URI);
-    console.log("Connected to MongoDB");
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "CafeRestApp API is running",
+  });
+});
 
-    await seedDefaultTables();
-    console.log('Seeding complete.');
-
-    app.use("/tables", tableRoutes);
-
-    app.get("/", (req, res) => {
-      res.status(200).json({
-        message: "CafeRestApp API is running",
-      });
-    });
-
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-
-  } catch (err) {
-    console.error("Could not connect to MongoDB:", err);
-  }
-}
-
-startServer();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
